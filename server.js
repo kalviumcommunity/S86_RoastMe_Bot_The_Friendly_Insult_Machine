ba6fa1a54979182ae8d70cea678f35fb106dc834
tanveer128423 <khalidtanveer653@gmail.com>
2025-02-12 13:20:03 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// /ping route
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
