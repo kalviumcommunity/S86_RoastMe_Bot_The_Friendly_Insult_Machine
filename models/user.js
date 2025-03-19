e44c157ec766e104f68d6e2dd0be9c26705f5a1d
tanveer128423 <khalidtanveer653@gmail.com>
2025-03-19 09:50:02 +0530

    updated

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('User', UserSchema);
